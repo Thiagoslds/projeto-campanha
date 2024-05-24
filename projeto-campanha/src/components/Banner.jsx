6c1c5660ecc3eb93a6dd78a5d972e72c1377cf9e
Thiago Souza Lopes Dauria Silveira <thiago.silveira@serpro.gov.br>
2024-05-24 16:34:31 -0300

    4#-Adiciona_banner

---
+++ projeto-campanha/src/components/Banner.jsx
@@ -0,0 +1,12 @@
+export default function Banner() {
+    return (
+        <>
+            <div id="banner">
+                <h1>A CAMPANHA PRECISA DE VOCÊ</h1>
+                <h3>Ajude nossa tarefa</h3>
+                <h4>Pix: campanha.paozinho@gmail.com</h4>
+                <button>Doar Agora</button>
+            </div>
+        </>
+    );
+}
