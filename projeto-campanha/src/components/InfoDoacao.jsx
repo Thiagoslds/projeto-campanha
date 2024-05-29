aa543798570e2fe608450af0efd0ebdabf985307
Thiago Souza Lopes Dauria Silveira <thiago.silveira@serpro.gov.br>
2024-05-29 17:28:22 -0300

    6#-Modal_doacao

--- projeto-campanha/src/components/InfoDoacao.jsx
+++ projeto-campanha/src/components/InfoDoacao.jsx
@@ -1,20 +1,34 @@
+import { useRef, useState } from "react";
+import ModalDoacao from "./ModalDoacao";
+
 export default function InfoDoacao() {
+    const dialogModal = useRef();
+    const [valuePix, setValuePix] = useState(0);
+
+    function handleClick(valuePixInHandle) {
+        setValuePix(valuePixInHandle);
+        dialogModal.current.showModal();
+    }
+
     return (
-        <div id="area-doacao">
-            <div className="area-doacao-info">
-                <h1>Escolha um valor e faça sua doação</h1>
-                <h3>Nós precisamos de doações para comprar os pães, o leite, cobertores para época de frio, produção de panfletos e muito mais.</h3>
-            </div>
-            <div className="area-doacao-blocos">
-                <div>
-                    <span>R$ 20,00</span>
-                    <span>R$ 50,00</span>
+        <>
+            <ModalDoacao ref={dialogModal} valuePix={valuePix}/>
+            <div id="area-doacao">
+                <div className="area-doacao-info">
+                    <h1>Escolha um valor e faça sua doação</h1>
+                    <h3>Nós precisamos de doações para comprar os pães, o leite, cobertores para época de frio, produção de panfletos e muito mais.</h3>
                 </div>
-                <div>
-                    <span>R$ 100,00</span>
-                    <span>R$ 200,00</span>
+                <div className="area-doacao-blocos">
+                    <div>
+                        <span onClick={() => { handleClick(20) }}>R$ 20,00</span>
+                        <span onClick={() => { handleClick(50) }}>R$ 50,00</span>
+                    </div>
+                    <div>
+                        <span onClick={() => { handleClick(100) }}>R$ 100,00</span>
+                        <span onClick={() => { handleClick(200) }}>R$ 200,00</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
---
+++ projeto-campanha/src/components/ModalDoacao.jsx
@@ -0,0 +1,53 @@
+import { forwardRef } from "react";
+import pix20 from "../assets/pix-20.jpeg";
+import pix50 from "../assets/pix-50.jpeg";
+import pix100 from "../assets/pix-100.jpeg";
+import pix200 from "../assets/pix-200.jpeg";
+
+
+const modalDoacao = forwardRef(
+    function ModalDoacao({ valuePix }, ref) {
+        let valuePixDefault = null;
+        let linePixPaste = '';
+
+        switch (valuePix) {
+            case 20:
+                valuePixDefault = pix20;
+                linePixPaste = "00020126490014br.gov.bcb.pix0127campanha.paozinho@gmail.com520400005303986540520.005802BR5925Vanessa Souza Lopes Silve6009Sao Paulo62070503***6304FAE7";
+                break;
+            case 50:
+                valuePixDefault = pix50;
+                linePixPaste = "00020126490014br.gov.bcb.pix0127campanha.paozinho@gmail.com520400005303986540550.005802BR5925Vanessa Souza Lopes Silve6009Sao Paulo62070503***63045E22";
+                break;
+            case 100:
+                valuePixDefault = pix100;
+                linePixPaste = "00020126490014br.gov.bcb.pix0127campanha.paozinho@gmail.com5204000053039865406100.005802BR5925Vanessa Souza Lopes Silve6009Sao Paulo62070503***63048EF9";
+                break;
+            case 200:
+                valuePixDefault = pix200;
+                linePixPaste = "00020126490014br.gov.bcb.pix0127campanha.paozinho@gmail.com5204000053039865406200.005802BR5925Vanessa Souza Lopes Silve6009Sao Paulo62070503***63042ED2";
+                break;
+            default:
+                break;
+        }
+
+        function copyText(valueToCopy) {
+            navigator.clipboard.writeText(valueToCopy);
+        }
+
+        return (
+            <dialog className="modal-doacao" ref={ref}>
+                <h2>Faz o Pix!</h2>
+                <p>É só apontar a câmera do seu celular para ler o QR Code!</p>
+                <img src={valuePixDefault} alt="QR-Code de pix" />
+                <p>Ou, se preferir, clique e copie a chave PIX abaixo:</p>
+                <p class="modal-doacao-line" onClick={() => { copyText(linePixPaste) }}>{linePixPaste}</p>
+                <form method="dialog">
+                    <button className="button-red">Fechar</button>
+                </form>
+            </dialog>
+        );
+    }
+);
+
+export default modalDoacao;
